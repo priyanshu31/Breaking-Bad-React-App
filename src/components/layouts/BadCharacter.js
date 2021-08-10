771c57d09780c6ea83e7eb1064a95620e98c8cf1
priyanshu31 <priyanshu@github.com>
2021-08-10 17:40:39 +0530

    Added Comment in BadCharacter.js

--- src/components/layouts/BadCharacter.js
+++ src/components/layouts/BadCharacter.js
@@ -1,32 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import BadCharacters from './BadCharacters'
+
+// Functional Component BadCharacter 
 
 const BadCharacter = ({ badCharacter }) => {
-    let flag = false;
+
+    // using badCharacter propa passed by BadCharacters Component to display the card
 
     return (
         
         <div className="card" style={{width: '16rem', margin: '1rem'}}>
+            
             <div className="card-body">
                 <h5 className="card-title">{badCharacter.name}</h5>
             </div>
+
             <ul className="list-group list-group-flush">
+                
                 <li className="list-group-item">
+                    {/* using .join function to join the element in array with ", " */}
                     <strong> Occupation: </strong> {badCharacter.occupation.join(", ")}
                 </li>
+
                 <li className="list-group-item">
                     <strong> Date of Birth: </strong> {badCharacter.birthday}
                 </li>
+
                 <li className="list-group-item">
                     <strong> Status: </strong> {badCharacter.status}
-                    </li>
+                </li>
+
             </ul>
+
+            {/* using Link to navigate to '/readmore' path when user click on readmore button */}
             <div className="card-body">
+
+                {/* passing badCharacter object in location along with pathname */}
                 <Link to={{pathname: '/readmore', badCharacter: badCharacter }} className="btn btn-primary">Read More...</Link>
+            
             </div>
+
         </div>
 
     )
 }
 
+
+// exporting BadCharacter Functional Component
 export default BadCharacter
