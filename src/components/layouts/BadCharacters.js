9ddf06565bb37d2cae7e01ad68f5c55602984d30
priyanshu31 <priyanshu@github.com>
2021-08-10 11:56:17 +0530

    Pagination Done

--- src/components/layouts/BadCharacters.js
+++ src/components/layouts/BadCharacters.js
@@ -1,27 +1,51 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import BadCharacter from './BadCharacter'
 import ReactPaginate from 'react-paginate';
 
 const BadCharacters = ({ badCharacters }) => {
 
+    const [pageNumber, setPageNumber] = useState(0)
+    
+    const charactersPerPage = 8
+    let pageVisited = pageNumber * charactersPerPage
+
+    const displayBadCharacters = badCharacters
+    .slice(pageVisited, pageVisited + charactersPerPage)
+    .map((badCharacter) => (
+        <BadCharacter key = { badCharacter.char_id } badCharacter = { badCharacter } />
+    ))
+
+    const changePage = ({ selected }) => setPageNumber(selected)
+
     let cardstyle = {
         padding: '1rem',
         display: 'grid',
         gridTemplateColumns: 'repeat(4, 1fr)',
-        gridGap: '1.2rem'
+        gridGap: '1.2rem',
     };
 
     if (badCharacters.length > 0)
         return (    
 
             <div className="container" style={cardstyle}>
-                {
+                {/* {
                     badCharacters.map((badCharacter) => (
                     
                         <BadCharacter key = { badCharacter.char_id } badCharacter = { badCharacter } />
 
                     ))
-                }
+                } */}
+                {displayBadCharacters}
+
+                <ReactPaginate 
+                    previousLabel = { "Previous" }
+                    nextLabel = { "Next" }
+                    pageCount = { Math.ceil(badCharacters.length / charactersPerPage) }
+                    onPageChange = { changePage }
+                    containerClassName = {"pagination"}
+                    subContainerClassName={"pages pagination"}
+                />
+
             </div>
 
         )
